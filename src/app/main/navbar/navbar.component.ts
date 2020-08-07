06f49a90da083dd2cbf2569701cd8ad7920d33f5
Andrii Krinskiy <andrewkrinskiy@gmail.com>
2020-08-07 18:35:23 +0300

    [update] navbar styles

--- src/app/main/navbar/navbar.component.ts
+++ src/app/main/navbar/navbar.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, AfterViewChecked } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewChecked {
 
-  constructor() { }
+  listName = 'list name';
+  dropdown = false;
+
+  constructor(
+    private cdRef:ChangeDetectorRef,
+    public auth: AuthService
+  ) { }
 
   ngOnInit(): void { }
+
+  ngAfterViewChecked(): void {
+    this.cdRef.detectChanges();
+  }
+
+  calcWidth(mask: HTMLElement, maxWidth = 500) {
+    return maxWidth > mask.offsetWidth ? mask.offsetWidth : maxWidth;
+  }
 
 }
